7e4170ff488d11b8b368674087cf87e4bed38b53
Vladimir Povalskij <lokireads@gmail.com>
2023-11-29 15:24:48 +0300

    create, add and delete cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,27 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+// @todo: Добавить еще один параметр в виде колбека
+const createCard = ({name, link}) => {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__delete-button').addEventListener('click', () => deleteCard(cardElement));
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+const deleteCard = cardElement => cardElement.remove();
 
 // @todo: Вывести карточки на страницу
+function loadCards() {
+    initialCards.forEach(item => {
+        placesList.append(createCard(item));
+    })
+}
+
+loadCards()
