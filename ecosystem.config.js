422346a9cf2595caeb6941a04e8bd00d5b02fb93
Vladimir Povalskij <lokireads@gmail.com>
2024-12-13 02:13:25 +0300

    Добавил деплой фронтенда

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+require('dotenv').config({path: '.env.deploy'});
+
+const { DEPLOY_USER, DEPLOY_HOST, DEPLOY_REF, DEPLOY_REPOSITORY, DEPLOY_PATH } = process.env;
+
+module.exports = {
+  apps : [{
+    name   : "mesto-backend",
+    script : "./dist/app.js"
+  }],
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: DEPLOY_REPOSITORY,
+      path: DEPLOY_PATH,
+      'post-deploy': 'npm ci --legacy-peer-deps && pwd && npm run build',
+    },
+  },
+}
